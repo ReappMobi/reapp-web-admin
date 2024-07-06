d17e2a80ca00a4f00e84427b508858a220a29707
Andre0n <andreon098@gmail.com>
2024-07-06 18:41:13 -0300

    feat: update login page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,9 +1,91 @@
 import React from 'react';
 
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+
+import { Button } from '@/components/ui/button';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+
+import { Input } from '@/components/ui/input';
+import { FormDescription } from '../components/ui/form';
+
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: 'Username must be at least 2 characters.',
+  }),
+  password: z.string().min(8, {
+    message: 'Password must be at least 8 characters.',
+  }),
+});
+
 export const Login: React.FC = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      password: '',
+      username: '',
+    },
+  });
+
+  const onSubmit = () => {
+    console.log();
+  };
+
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <main className="h-screen flex items-center justify-center flex-col">
+      <h1 className="text-3xl font-bold text-center text-reapp-secondary">
+        Login
+      </h1>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4 w-96">
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="font-semibold">Username</FormLabel>
+                <FormControl>
+                  <Input placeholder="Username" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="font-semibold">Password</FormLabel>
+                <FormControl>
+                  <Input placeholder="**********" type="password" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormDescription className="text-center">
+            Tendo problemas para acessar sua conta?
+            <a href="/troble" className="text-reapp-primary">
+              Click aqui
+            </a>
+          </FormDescription>
+          <Button
+            type="login"
+            className="w-full bg-reapp-primary hover:bg-reapp-secondary"
+          >
+            Login
+          </Button>
+        </form>
+      </Form>
+    </main>
   );
-}
+};
